c61cc61aecce1cd0199a7ce283ebd9bf41027b14
favour <ogboifavourifeanyichukwu>
2022-12-15 04:30:29 +0100

    machine learning

---
+++ deploymodel.py
@@ -0,0 +1,89 @@
+from lib2to3.pgen2.pgen import DFAState
+import streamlit as st
+from PIL import Image
+import pandas as pd
+import pickle
+import numpy as np
+
+@st.cache
+
+def loadimage(imagefile):
+    img = Image.open(imagefile)
+    return img
+
+def main():
+    st.title("Diabeties Prediction")
+
+    menu = ["About","Dataset","PredictionApp"]
+    choice = st.sidebar.selectbox("Menu",menu)
+
+    if choice == "Dataset":
+
+        st.write("The dataset is shown below.")
+
+        st.subheader("Dataset")
+        datafile = ("C:/Users/Master/Desktop/Diabetes_Prediction_Using_Learning_With_Python/diabetes.csv")
+        df = pd.read_csv(datafile)
+        st.dataframe(df)
+
+        st.write("Correlation As you can tell from the analysis the values in some columns are more closely related to the outcome compared to other columns. This relationship is often expressed numerically using a measure called the _correlation coefficient_, which can be computed using the .corr method of a Pandas series.")
+
+        imagefile = ("C:/Users/Master/Desktop/Diabetes_Prediction_Using_Learning_With_Python/images/corr.PNG")
+        st.image(loadimage(imagefile),width = 400)
+
+        
+
+    elif choice == "PredictionApp":
+        st.subheader("PredictionApp")
+
+        # loading the saved model
+
+        loaded_model = pickle.load(open("C:/Users/Master/Desktop/Diabetes_Prediction_Using_Learning_With_Python/trained_model.sav","rb"))
+
+        Pregnancies = st.text_input("Number Of Pregnancies")
+        Glucose = st.text_input("glucose Level")
+        BloodPressure = st.text_input("Blood pressure value")
+        SkinThickness = st.text_input("Skin Thickness Value")
+        Insulin = st.text_input("Insulin Level")
+        BMI = st.text_input("BMI Level")
+        DiabetesPedigreeFunction = st.text_input("Diabetes Pedigree Function Value")
+        Age = st.text_input("Age Value")
+
+        ans = ""
+
+        # Creating a button for prediction
+        if st.button("Diabetes Test Result"):
+            input_data = [Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]
+
+            # changin the input data to a numpy array
+
+            input_data_as_numpy_array = np.asarray(input_data)
+
+            # reshape the array as we are predicting for only one instance
+
+            input_data_reshape = input_data_as_numpy_array.reshape(1,-1)
+
+            prediction = loaded_model.predict(input_data_reshape)
+
+            print('The prediction is :', prediction)
+
+            if prediction[0] == 0:
+                ans = ("The Person Is Not Diabetic")
+            else:
+                ans = ("The Person Is Diabetic")
+
+        st.success(ans)
+
+                
+            
+    else:
+        st.subheader("About")
+        imagefile = ("C:/Users/Master/Desktop/Diabetes_Prediction_Using_Learning_With_Python/images\pexels-artem-podrez-6823763.jpg")
+        st.image(loadimage(imagefile),width = 400)
+
+        txtfile = ("Diabetes mellitus is a condition defined by persistently high levels of sugar (glucose) in the blood There are several types of diabetes. The two most common are called type 1 diabetes and type 2 diabetes.During digestion, food is broken down into its basic components. Carbohydrates are broken down into simple sugars, primarily glucose. Glucose is a critically important source of energy for the body’s cells.To provide energy to the cells, glucose needs to leave the bloodstream and get inside the cells.An organ in the abdomen called the pancreas produces a hormone called insulin, which is essential to helping glucose get into the body's cells. In a person without diabetes,the pancreas produces more insulin whenever blood levels of glucose rise (for example, after a meal),and the insulin signals the body's cells to take in the glucose. In diabetes,either the pancreas's ability to produce insulin or the cells' response to insulin is altered.The purpose of this app is to deploy a model that can predict if a person is diabetic or not based of the information given by th user.")
+        st.write(txtfile)
+
+
+if __name__ == '__main__':
+    main()
