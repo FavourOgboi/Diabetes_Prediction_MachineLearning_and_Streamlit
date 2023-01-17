bfced2999d94ff7bb9c62d43a3420698571e996e
Ogboi Favour <ogboifavourifeanyichukwu@gmail.com>
2023-01-17 20:42:22 +0100

    Update deploymodel.py

--- deploymodel.py
+++ deploymodel.py
@@ -1,12 +1,19 @@
-from lib2to3.pgen2.pgen import DFAState
 import streamlit as st
 from PIL import Image
 import pandas as pd
 import pickle
 import numpy as np
 
-@st.cache
+# use the current working directory to define the path of the images folder
+image_folder = "images"
 
+# use the current working directory to define the path of the diabetes.csv file
+datafile = "diabetes.csv"
+
+# use the current working directory to define the path of the trained_model.sav file
+trained_model = "trained_model.sav"
+
+@st.cache
 def loadimage(imagefile):
     img = Image.open(imagefile)
     return img
@@ -22,23 +29,22 @@ def main():
         st.write("The dataset is shown below.")
 
         st.subheader("Dataset")
-        datafile = ("C:/Users/Master/Desktop/Diabetes_Prediction_Using_Learning_With_Python/diabetes.csv")
         df = pd.read_csv(datafile)
         st.dataframe(df)
 
-        st.write("Correlation As you can tell from the analysis the values in some columns are more closely related to the outcome compared to other columns. This relationship is often expressed numerically using a measure called the _correlation coefficient_, which can be computed using the .corr method of a Pandas series.")
-
-        imagefile = ("C:/Users/Master/Desktop/Diabetes_Prediction_Using_Learning_With_Python/images/corr.PNG")
-        st.image(loadimage(imagefile),width = 400)
-
+        st.write("Correlation As you can tell from the analysis the values in some columns are more closely related to the outcome compared to other columns." +
         
+        "This relationship is often expressed numerically using a measure called the _correlation coefficient_, which can be computed using the .corr method of a Pandas series.")
+
+        # use the current working directory to define the path of the corr.PNG file
+        imagefile = f"{image_folder}/corr.PNG"
+        st.image(loadimage(imagefile),width = 400)
 
     elif choice == "PredictionApp":
         st.subheader("PredictionApp")
 
         # loading the saved model
-
-        loaded_model = pickle.load(open("C:/Users/Master/Desktop/Diabetes_Prediction_Using_Learning_With_Python/trained_model.sav","rb"))
+        loaded_model = pickle.load(open(trained_model,"rb"))
 
         Pregnancies = st.text_input("Number Of Pregnancies")
         Glucose = st.text_input("glucose Level")
@@ -74,16 +80,18 @@ def main():
 
         st.success(ans)
 
-                
-            
     else:
         st.subheader("About")
-        imagefile = ("C:/Users/Master/Desktop/Diabetes_Prediction_Using_Learning_With_Python/images/pexels-artem-podrez-6823763.jpg")
-        st.image(loadimage(imagefile),width = 300)
+        # use the current working directory to define the path of the pexels-artem-podrez-6823763.jpg file
+        imagefile = f"{image_folder}/pexels-artem-podrez-6823763.jpg"
+        st.image(loadimage(imagefile),width = 500)
 
-        txtfile = ("Diabetes mellitus is a condition defined by persistently high levels of sugar (glucose) in the blood There are several types of diabetes. The two most common are called type 1 diabetes and type 2 diabetes.During digestion, food is broken down into its basic components. Carbohydrates are broken down into simple sugars, primarily glucose. Glucose is a critically important source of energy for the body cells.To provide energy to the cells, glucose needs to leave the bloodstream and get inside the cells.An organ in the abdomen called the pancreas produces a hormone called insulin, which is essential to helping glucose get into the body's cells. In a person without diabetes,the pancreas produces more insulin whenever blood levels of glucose rise (for example, after a meal),and the insulin signals the body's cells to take in the glucose. In diabetes,either the pancreas's ability to produce insulin or the cells' response to insulin is altered.The purpose of this app is to deploy a model that can predict if a person is diabetic or not based of the information given by th user.")
-        st.write(txtfile)
+        txtfile = "about.txt"
+        with open(txtfile, "r") as f:
+            about_text = f.read()
+        st.write(about_text)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+
+
